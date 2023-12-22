f0c97efd083301c0f7af1b9be5ff8e99fc25176b
Роман Хайдаров <romanhajdarov@MacBook-Pro-Roman-2.local>
2023-12-23 01:21:37 +0600

    add main structs for api

---
+++ todo.go
@@ -0,0 +1,26 @@
+package todo
+
+type TodoList struct {
+		Id int 							`json:"-"`
+		Title string 				`json:"title"`
+		Description string	`json:"body"`
+}
+
+type UserList struct {
+		Id int
+		UserId int
+		ListId int
+}
+
+type TodoItem struct {
+		Id int 							`json:"-"`
+		Title string 				`json:"title"`
+		Description string	`json:"body"`
+		Done bool						`json:"done"`
+}
+
+type ListsItem struct {
+	Id int
+	ListId int
+	ItemId string
+}
